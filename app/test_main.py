dc46bf19bf05515b54ddb6c85d1989a687916b60
Mariana Lishchynska <lishchynska.m@gmail.com>
2024-12-16 14:07:56 +0200

    'Fixed'

--- app/test_main.py
+++ app/test_main.py
@@ -12,6 +12,6 @@ from app.main import get_coin_combination
         (50, [0, 0, 0, 2]),
     ]
 )
-def test_get_coin_combination(number_of_coins: int, expected_result: list[int]) -> None:
+def test_get_coin_combination(number_of_coins: int,
+                              expected_result: list[int]) -> None:
     assert get_coin_combination(number_of_coins) == expected_result
-
